e2b8c165f204e2bdcb1c08fe59c0c43ceefcf38e
Chris Hausky <contact@krzyhau.pl>
2025-10-07 16:01:13 +0200

    begin reworking projection physics

--- Assets/Scripts/FezPhysicsObject.cs
+++ Assets/Scripts/FezPhysicsObject.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
 [RequireComponent(typeof(Rigidbody))]
 public class FezPhysicsObject : MonoBehaviour
 {
+    private const float CONTACT_OFFSET = 0.01f;
+    private static RaycastHit[] _raycastHitBuffer = new RaycastHit[256];
+    
     [SerializeField] private BoxCollider _collider;
     [SerializeField] private Rigidbody _rigidbody;
 
@@ -13,136 +20,164 @@ public class FezPhysicsObject : MonoBehaviour
 
     private void FixedUpdate()
     {
+        DEBUG_Update();
         Handle2DMovement();
     }
 
     private void Handle2DMovement()
     {
-        Vector3 castDir = Quaternion.Euler(0.0f, LevelManager.Camera.PhysicsAngle, 0.0f) * Vector3.forward;
-
-        // handle 2D landing before all else - we don't want to phase through the floor when going down
-        Handle2DLanding(castDir);
-        Handle2DLanding(castDir * -1);
-
-        for (int i = -1; i <= 1; i++)
+        if (Mathf.Approximately(_rigidbody.linearVelocity.sqrMagnitude, 0f))
         {
-            // handle wall movement for forward faces.
-            Handle2DWallMovement(castDir, i);
-            // doing the same for back walls, except check for blocked flat jump (indoors movement)
-            Handle2DWallMovement(-castDir, i, false);
+            return;
         }
-    }
+        
+        Vector3 projectionNormal = GetMovementProjectionNormal();
 
-    // handle 2D movement "through" walls
-    private bool Handle2DWallMovement(Vector3 castDir, int vOffset, bool allowFlatJump = true)
+        Vector3 movementAdjustmentProjectionNormal = projectionNormal * (IsCovered(projectionNormal) ? -1.0f : 1.0f);
+        AdjustForProjectedMovement(movementAdjustmentProjectionNormal);
+    }
+    
+    private Vector3 GetMovementProjectionNormal()
     {
-        // doing two raycasts: one to detect if we're not already obscured by a wall
-        // if we do, doing another one to check if we will be obscured by a wall
-        float hitboxWidth = _collider.size.x;
-        Vector3 startPos = transform.position - castDir * _2DCastDistance + Vector3.up * vOffset * _collider.size.y * 0.49f;
-        if (vOffset == 0) startPos += Vector3.down * 0.005f;
-        RaycastHit hit;
-        for (int i = 0; i < 2; i++)
-        {
-            float castLength = _2DCastDistance + hitboxWidth * 0.4999f;
-            if (i == 1)
-            {
-                Vector3 moveVel = _rigidbody.linearVelocity;
-                moveVel.y = Mathf.Max(moveVel.y, 0);
-                Vector3 horVer = new Vector3(moveVel.x, 0, moveVel.z);
-                if (horVer.magnitude != 0) startPos += horVer.normalized * hitboxWidth * 0.5f;
-                startPos += moveVel * Time.fixedDeltaTime;
-                castLength = _2DCastDistance + hitboxWidth * 0.5001f;
-            }
-            RaycastHit[] hits = Physics.RaycastAll(startPos, castDir, castLength, _groundMask, QueryTriggerInteraction.Ignore);
-            if (hits.Length > 0)
-            {
-                hit = hits[0];
-                foreach (RaycastHit h in hits)
-                {
-                    if (h.distance > hit.distance) hit = h;
-                }
-                float hitForce = -Vector3.Dot(hit.normal, castDir);
-                if (hitForce > 0.01)
-                {
-                    if (i == 0)
-                    {
-                        Debug.DrawLine(hit.point, hit.point + hit.normal, Color.red);
-                        return false;
-                    } else
-                    {
-                        float spaceBetweenWall = 1.1f;
-                        float flatJumpForce = (_2DCastDistance - hit.distance + hitboxWidth * 0.5f * (spaceBetweenWall + (1f - hitForce) * 2.0f));
-                        // additional raycast to check if illegal jump occurs
-                        if (!allowFlatJump)
-                        {
-                            RaycastHit flatHit;
-                            Vector3 flatStartPos = startPos + castDir * (_2DCastDistance - hitboxWidth * 0.5f);
-                            bool blockedFlatJump = Physics.Raycast(flatStartPos, -castDir, out flatHit, flatJumpForce, _groundMask, QueryTriggerInteraction.Ignore);
-                            if (blockedFlatJump)
-                            {
-                                Debug.DrawLine(hit.point, hit.point + hit.normal, new Color(1.0f, 0.5f, 0.0f));
-                                Debug.DrawLine(flatHit.point, flatHit.point + flatHit.normal, new Color(1.0f, 0.5f, 0.5f));
-                                return false;
-                            }
-                        }
-                        //block jump if there is no space
-                        bool blockedSpace = Physics.Raycast(hit.point, hit.normal, out var spaceHit, hitboxWidth, _groundMask, QueryTriggerInteraction.Ignore);
-                        if (blockedSpace)
-                        {
-                            Debug.DrawLine(hit.point, spaceHit.point, new Color(0.5f, 0f, 0f));
-                            return false;
-                        }
-
-                        Debug.DrawLine(hit.point, hit.point + hit.normal, Color.green);
-                        transform.position -= castDir * flatJumpForce;
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+        return Quaternion.Euler(0.0f, LevelManager.Camera.PhysicsAngle, 0.0f) * Vector3.forward;
     }
 
-    private void Handle2DLanding(Vector3 castDir)
+    private bool IsCovered(Vector3 projectionAxis)
     {
-        if (_rigidbody.linearVelocity.y >= 0) return;
-
-        //check if we're not about to land on a ground
-        bool aboutToLand = Physics.CheckBox(
-            transform.position - new Vector3(0, _collider.size.y * 0.5f, 0),
-            new Vector3(_collider.size.x * 0.499f, Mathf.Abs(_rigidbody.linearVelocity.y) * Time.fixedDeltaTime * 2.1f, _collider.size.z * 0.499f),
-            Quaternion.identity,
-            _groundMask
-        );
-
-        if (aboutToLand) return;
-
-        // now we can actually do the landing logic
-        Vector3 startPos = transform.position + Vector3.down * _collider.size.y * 0.5f;
-        Vector3 startPosAfterMove = startPos + _rigidbody.linearVelocity * Time.fixedDeltaTime;
-
-        Vector3 sideVec = Vector3.Cross(Vector3.up, castDir.normalized);
-        for (int i = -1; i <= 1; i += 2)
+        var fromBackOrigin = _rigidbody.position - projectionAxis * _2DCastDistance;
+        float spaceFromBack = MeasureObjectBoxCast(fromBackOrigin, projectionAxis, _2DCastDistance);
+        DEBUG_DrawBox(Color.blue, fromBackOrigin + projectionAxis * spaceFromBack);
+        return spaceFromBack < _2DCastDistance - _collider.size.z * 0.5f;
+    }
+    
+    private void AdjustForProjectedMovement(Vector3 projectionAxis)
+    {
+        var backwardsAxis = projectionAxis * -1.0f;
+        var availableSpace = MeasureObjectBoxCast(_rigidbody.position, backwardsAxis, _2DCastDistance);
+        
+        var movementStep = GetProjectedMovementStep(projectionAxis);
+        var movementStepDistance = movementStep.magnitude + CONTACT_OFFSET;
+        var movementStepDirection = movementStep.normalized;
+        
+        var potentialWarpPlacesFindOrigin = _rigidbody.position + backwardsAxis * availableSpace + movementStep;
+        
+        var bestPlaceAdjustmentDistance = 0f;
+        var bestPlaceDistanceToCollision = MeasureObjectBoxCast(_rigidbody.position, movementStepDirection, movementStepDistance);
+        
+        foreach (var potentialWarpPlaceHit in IterateObjectBoxCasts(potentialWarpPlacesFindOrigin, projectionAxis, availableSpace))
         {
-            Vector3 o = sideVec * i * _collider.size.x * 0.49f;
-            bool foundGround = Physics.Raycast(startPosAfterMove + o, castDir, out var groundHit, _2DCastDistance, _groundMask, QueryTriggerInteraction.Ignore);
-            if (foundGround)
+            var potentialAdjustmentDistance = availableSpace - potentialWarpPlaceHit.distance + CONTACT_OFFSET;
+            var potentialWarpPlaceCheckOrigin = _rigidbody.position + backwardsAxis * potentialAdjustmentDistance;
+            
+            var distanceToCollision = MeasureObjectBoxCast(potentialWarpPlaceCheckOrigin, movementStepDirection, movementStepDistance);
+
+            var collidedFromMovement = distanceToCollision < movementStepDistance;
+            var isBetterWarpPlace =
+                (!collidedFromMovement && potentialAdjustmentDistance > bestPlaceAdjustmentDistance) ||
+                (collidedFromMovement && distanceToCollision - CONTACT_OFFSET > bestPlaceDistanceToCollision);
+            
+            DEBUG_DrawBox(Color.darkGreen, potentialWarpPlaceCheckOrigin);
+            DEBUG_DrawBox(Color.green, potentialWarpPlaceCheckOrigin + movementStepDirection * distanceToCollision);
+            DEBUG_DrawText(potentialWarpPlaceCheckOrigin, $"Collision factor: {distanceToCollision / movementStepDistance}");
+            
+            if (isBetterWarpPlace)
             {
-                bool foundFar = Physics.BoxCast(
-                    startPos + Vector3.up * 0.1f,
-                    new Vector3(_collider.size.x, 0.1f, 0.1f),
-                    castDir, out var farHit, Camera.main.transform.rotation,
-                    _2DCastDistance, _groundMask, QueryTriggerInteraction.Ignore
-                );
-                if (!foundFar || farHit.distance - _collider.size.x > groundHit.distance)
-                {
-                    transform.position = transform.position + castDir.normalized * (groundHit.distance + _collider.size.x * 0.51f);
-                    Debug.DrawLine(groundHit.point, groundHit.point + groundHit.normal, Color.cyan);
-                    Debug.DrawLine(groundHit.point, startPos + o, Color.cyan);
-                    return;
-                }
+                bestPlaceAdjustmentDistance = potentialAdjustmentDistance;
+                bestPlaceDistanceToCollision = distanceToCollision;
             }
         }
+        
+        _rigidbody.position += backwardsAxis * bestPlaceAdjustmentDistance;
     }
+
+    private Vector3 GetProjectedMovementStep(Vector3 projectionAxis)
+    {
+        var projectedMovement = Vector3.ProjectOnPlane(_rigidbody.linearVelocity, projectionAxis);
+        
+        var projectedMovementDirection = projectedMovement.normalized;
+        var projectedMovementDistance = projectedMovement.magnitude * Time.fixedDeltaTime + CONTACT_OFFSET;
+        
+        return projectedMovementDirection * projectedMovementDistance;
+    }
+    
+    private float MeasureObjectBoxCast(Vector3 origin, Vector3 direction, float distance)
+    {
+        float minDistance = distance;
+        foreach (var hit in IterateObjectBoxCasts(origin, direction, distance))
+        {
+            minDistance = Mathf.Min(minDistance, hit.distance - CONTACT_OFFSET);
+        }
+        return minDistance;
+    }
+    
+    private IEnumerable<RaycastHit> IterateObjectBoxCasts(Vector3 origin, Vector3 direction, float distance)
+    {
+        var adjustedColliderSize = _collider.size - Vector3.one * CONTACT_OFFSET;
+        
+        var hitCount = Physics.BoxCastNonAlloc(origin, adjustedColliderSize * 0.5f, direction, 
+            _raycastHitBuffer, transform.rotation, distance, _groundMask);
+        
+        for (var i = 0; i < hitCount; i++)
+        {
+            var hit = _raycastHitBuffer[i];
+            if (Mathf.Approximately(hit.distance, 0f) || hit.collider == _collider)
+            {
+                continue;
+            }
+            yield return _raycastHitBuffer[i];
+        }
+    }
+
+    
+#if UNITY_EDITOR
+    private readonly List<(Color Color, Vector3 Position, float UntilTimestamp)> _debugBoxBuffer = new();
+    private readonly List<(Vector3 Position, string Text, float UntilTimestamp)> _debugTextBuffer = new();
+    
+    private void DEBUG_DrawBox(Color color, Vector3 position, float time = 0.0f)
+    {
+        _debugBoxBuffer.Add((color, position, Time.time + time));
+    }
+
+    private void DEBUG_DrawText(Vector3 position, string text, float time = 0.0f)
+    {
+        _debugTextBuffer.Add((position, text, Time.time + time));
+    }
+    
+    private void OnDrawGizmos()
+    {
+        foreach (var debugBox in _debugBoxBuffer)
+        {
+            Gizmos.color = debugBox.Color;
+            Gizmos.DrawWireCube(debugBox.Position, _collider.size);
+        }
+        
+        foreach (var debugText in _debugTextBuffer)
+        {
+            UnityEditor.Handles.Label(debugText.Position, debugText.Text, DEBUG_GetTextStyle());
+        }
+    }
+
+    private GUIStyle DEBUG_GetTextStyle()
+    {
+        return new GUIStyle()
+        {
+            alignment = TextAnchor.MiddleCenter,
+            fontSize = 20,
+            normal = new GUIStyleState()
+            {
+                textColor = Color.white
+            }
+        };
+    }
+
+    private void DEBUG_Update()
+    {
+        _debugBoxBuffer.RemoveAll(debugBox => Time.time >= debugBox.UntilTimestamp);
+        _debugTextBuffer.RemoveAll(debugText => Time.time >= debugText.UntilTimestamp);
+    }
+#else
+    private void DEBUG_DrawText(Vector3 p, string t, float t = 0.0f) {}
+    private void DEBUG_DrawBox(int i, Color c, Vector3 p, float t = 0.0f) { }
+    private void DEBUG_Update() { }
+#endif
 }
